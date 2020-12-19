e7a6de388772469a3890c5116a1509eb8ed5e216
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-19 10:41:41 +0800

    test

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+// routes
+app.get('/',(req,res)=>{
+    res.send("1st route");
+})
+
+app.get('/posts',(req,res)=>{
+    res.send("1st route");
+})
+
+const port = 3000
+app.listen(port, ()=>{
+    console.log("listening on "+ port);
+})
