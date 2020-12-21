d39aae56cf709605adfd58434c6caaa9f8dadbb7
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-21 15:34:33 +0800

    stuff

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+// import routes
+const postsRoute = require('./routes/posts')
+app.use('/posts', postsRoute);
 
 // routes
-app.get('/',(req,res)=>{
-    res.send("1st route");
-})
+app.get("/", (req, res) => {
+  res.send("1st route");
+});
 
-app.get('/posts',(req,res)=>{
-    res.send("1st route");
-})
 
-const port = 3000
-app.listen(port, ()=>{
-    console.log("listening on "+ port);
-})
+//connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("connected to DB")
+);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("listening on " + port);
+});
