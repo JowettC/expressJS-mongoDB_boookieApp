0081bef90602f26138fdb974498eb25bfa2e2463
JowettC <jowettchng@outlook.com>
2021-01-03 18:01:43 +0800

    register fixed

--- routes/user.js
+++ routes/user.js
@@ -1,51 +1,38 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const Post = require('../models/Post')
-const bp = require('body-parser')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const bp = require("body-parser");
 // Database setup
+const User = require("../models/User");
 const db = require("../db");
+const { Mongoose } = require("mongoose");
 const dbName = "Boookie";
 const collectionName = "user";
 
-
+router.use(express.json());
 router.use(bp.json());
 
-db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
-  // get all items
-  router.get("/", (req, res) => {
-      console.log(collectionName)
-    dbCollection.find(collectionName).toArray(function(err, result) {
-      if (err) throw err;
-        res.send(result);
+router.post("/register", async (req, res) => {
+  //   try {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const username = req.body.username;
+  const usersign = { name: username };
+  const accessToken = jwt.sign(usersign, process.env.ACCESS_TOKEN_SECRET);
+  const myuser = new User({
+    username: req.body.username,
+    password: hashedPassword,
   });
-  
-  });
-  
-
-  // << db CRUD routes >>
-
-}, function(err) { // failureCallback
-  throw (err);
+  console.log(myuser);
+  await myuser.save();
+  res.send({ error: false, user: myuser, token: accessToken });
 });
-// router.get("/", (req, res) => {
-//     res.send("getting books");
-//   });
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    (req,res)=>{
+        console.log("connect to db")
+    }
+)
 
-router.post("/", (req, res) => {
-  const post = new Post({
-    title: req.body.title,
-    author: req.body.author,
-    date: Date.now()
-  });
-
-    post.save()
-    .then( data => {
-      // res.send({error:"false", data:data, msg:"created"});
-      res.json(data);
-    })
-    .catch(err =>{
-      res.send({error:"true", data:data, msg:"unable to create"})
-    })
-});
-
-  module.exports = router;
+module.exports = router;
