1aeaafb98dae1e16d8bf76c1ada69a960c40ccfe
JowettC <jowettchng@outlook.com>
2021-01-03 21:14:17 +0800

    cors policy

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const cors = require('cors')
 require("dotenv/config");
-
+app.use(cors());
 // import routes
 const booksRoute = require("./routes/books");
 const userRoute = require("./routes/user");
 app.use("/books", booksRoute);
 app.use("/user", userRoute);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
